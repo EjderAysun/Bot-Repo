54f5cd15711f8f7cffa6555dd764950a141b3bb4
Ejder Aysun <ejderaysunn@gmail.com>
2024-09-22 22:41:23 +0300

    bot eklendi

---
+++ ayarlar.py
@@ -0,0 +1,6 @@
+#settings.py in original
+
+ayarlar = {
+    "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
+    "TOKEN": "MTI4NzQ4MTc4NTI4NTg3Mzc1Ng.GbX3c7.9zbvFyRZj2j2Y49ENNmwqusQDBEPPbQJQHpl5U"
+}
